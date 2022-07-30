b651a1d61cb9a8eec40697f55e3a6787bb60be07
Pardhu5555 <gundlapallipardhu@gmail.com>
2022-07-30 21:55:53 +0100

    added file

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("pardhu")
+import pandas as pd
+import numpy as np
+import  nltk
+
+import os
+
+train_data = pd.read_csv("train.csv")
+test_data = pd.read_csv("test.csv")
+
+print(train_data.head())
+print(test_data.head())
+
+print(train_data.shape())
+print(test_data.shape())
+
